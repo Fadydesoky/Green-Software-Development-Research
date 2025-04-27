d0a8ced02aaa0bff0dae5af391c6e390530f4eba
Fadydesoky <119536601+Fadydesoky@users.noreply.github.com>
2025-04-27 17:24:09 +0300

    Columns in the dataset
    Head of the dataset

---
+++ Analytics-P3.py
@@ -0,0 +1,10 @@
+# Check what columns are available in the dataset
+print('Columns in the dataset:')
+print(df.columns.tolist())
+
+print('\
+Head of the dataset:')
+print(df.head())
+
+print('\
+Completed checking columns and head of the dataset.')
